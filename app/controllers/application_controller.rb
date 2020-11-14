ab397e7eafaa7228fe97e43ba36a7e9d4f1f34b7
Jenise Ruehle <jeniseruehle@gmail.com>
2020-11-14 13:14:56 -0600

    setting up controllers and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,21 @@ class ApplicationController < Sinatra::Base
     end
 
     get "/" do
-    
+        if logged_in?
+            redirect "/users/#{current_user.id}"
+        else
+            erb :welcome
+        end
+    end
+
+    helpers do
+        def logged_in?
+            !!current_user
+        end
+
+        def current_user
+            User.find_by(id: session[:user_id])
+        end
     end 
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,31 @@
 class UserController < ApplicationController
+
+    get '/signup' do
+        if logged_in?
+            redirect "/habits"
+        else
+            erb :'users/create_user'
+        end
+    end
+    
+    post '/signup' do
+
+    end
+    
+    get '/login' do
+        erb :login
+    end
+
+    post '/login' do
+        #authenticate my user
+        #log in
+        #redirect to user account
+        #error message if fail to login
+        #redirect to login
+    end
+
+
+
+
+
 end
